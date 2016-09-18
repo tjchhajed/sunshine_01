96ba5900a2cbeb6b37d804b8268576073e778303
Tejashree Chhajed <hello@tejashree.de>
2016-09-18 12:58:48 +0200

    added hello_World and textView

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
             return rootView;
         }

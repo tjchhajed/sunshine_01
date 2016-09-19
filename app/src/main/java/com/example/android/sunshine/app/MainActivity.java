018ebdc3be04db7a24491739f2d57aa7ce83cd75
Tejashree Chhajed <hello@tejashree.de>
2016-09-19 18:39:37 +0200

    Displayed list of weather data in a activity

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -58,6 +63,20 @@ public class MainActivity extends ActionBarActivity {
                                  Bundle savedInstanceState) {
 
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            List<String> weather = new ArrayList<String>();
+            weather.add("Today: Sunny-66/83");
+            weather.add("Tomorrow: Rain-22/45");
+            weather.add("Wednesday: Foggy-45/56");
+            weather.add("Thursday: Cloudy-56/89");
+            weather.add("Friday: Cloudy-45/67");
+
+
+            ArrayAdapter<String> weatherAdapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,weather);
+
+            ListView weatherView  = (ListView) rootView.findViewById(R.id.listView_forecast);
+            weatherView.setAdapter(weatherAdapter);
+
             return rootView;
         }
     }
